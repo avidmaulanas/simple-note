a8abb3c077459c4f605986d8e01e6ab6b12e40c0
avid <avid@41studio.com>
2015-08-05 21:46:04 +0700

    fix omniauth facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"]
-  provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
-  provider :google, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
+  provider  :facebook, ENV["FACEBOOK_KEY"], ENV["FACEBOOK_SECRET"],
+            scope: 'email', info_fields: 'email, name', display: 'popup'
+  provider  :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"]
+  provider  :google, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"]
 end
