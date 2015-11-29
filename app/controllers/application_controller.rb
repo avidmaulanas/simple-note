1b94460dbc9c7645475ca61b6109807519912195
Avid Maulana Saputra <avidmaulanas@gmail.com>
2015-11-29 18:58:30 +0700

    Fixed login form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,20 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+ 
+  def resource
+    @resource ||= User.new
+  end
+ 
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+  
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:signin)}
