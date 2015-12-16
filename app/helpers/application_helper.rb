b178bf338fc6d0da0b0f3b99fcf19cf14b8c1ce3
Avid Maulana Saputra <avidmaulanas@gmail.com>
2015-12-16 23:11:14 +0700

    Create alert notify

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,24 @@ module ApplicationHelper
 			"on #{date.strftime("%d %b %Y &#8226; %H:%M")}".html_safe
 		end
 	end
+
+	def alert_for(flash_type)  
+	  { success: 'alert-success',
+	    error: 'alert-danger',
+	    alert: 'alert-warning',
+	    notice: 'alert-info'
+	  }[flash_type.to_sym] || flash_type.to_s
+	end 
+
+	def flash_messages
+		flash.each do |name, msg|
+      concat(content_tag(:div, msg, class: [:alert, alert_for(name)]) do
+        concat(content_tag(:button, type: "button", class: "close", data: {dismiss: "alert"}, aria: {label: "Close"}) do
+          concat content_tag(:span, '&times;'.html_safe, aria: {hidden: "true"})
+        end)
+        concat msg
+      end)
+    end
+    nil
+	end
 end
